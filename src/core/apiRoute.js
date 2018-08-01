f3125bfbdf7abd18eb0dc58198cc951fc4c30503
Eore <eore26@gmail.com>
2018-08-01 18:10:21 +0700

    add api router

--- src/core/apiRoute.js
+++ src/core/apiRoute.js
@@ -1,1 +1,14 @@
-let apiUser = require("../api/user");
+let fs = require("fs");
+
+module.exports = app => {
+  let api = fs.readdirSync("./src/api");
+  api.forEach(route => {
+    require("../api/" + route).forEach(val => {
+      app[val.method.toLocaleLowerCase()](
+        "/api/v1" + val.url,
+        require("./controllers/" + val.controller)
+      );
+    });
+  });
+  return app;
+};
