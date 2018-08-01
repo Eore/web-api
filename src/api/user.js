eafa0dbaa3ab7d77d28bfe13b1fec02813e10481
Eore <eore26@gmail.com>
2018-08-01 18:10:37 +0700

    update user api endpoint

--- src/api/user.js
+++ src/api/user.js
@@ -2,19 +2,25 @@ module.exports = [
   {
     method: "GET",
     url: "/user",
-    controller: "user/userList",
+    controller: "user/list",
+    privillage: ["admin"]
+  },
+  {
+    method: "GET",
+    url: "/user/:username",
+    controller: "user/find",
     privillage: ["admin"]
   },
   {
     method: "POST",
     url: "/user",
-    controller: "user/userAdd",
-    privillage: ["user", "guest"]
+    controller: "user/add",
+    privillage: ["guest"]
   },
   {
     method: "PUT",
-    url: "/user/:id",
-    controller: "user/userEdit",
+    url: "/user/:username",
+    controller: "user/edit",
     privillage: ["user"]
   }
 ];
