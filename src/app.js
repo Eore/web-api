06ee447b5762b6b88e84714e4e7d0c7e3ffdd033
Eore <eore26@gmail.com>
2018-08-01 18:09:04 +0700

    add middleware and api route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+let http = require("http");
 let express = require("express");
 let app = express();
 let port = process.env.PORT || 8000;
-app.listen(port);
+app = require("./deps/middleware")(app);
+app = require("./core/apiRoute")(app);
+http
+  .createServer(app)
+  .listen(port, () => console.log(`Server run in port ${port}`));
