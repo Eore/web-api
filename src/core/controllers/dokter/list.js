4c5f009df77f26a4e27ca38c1e7d2d608fa03126
Eore <eore26@gmail.com>
2018-08-02 18:40:12 +0700

    add list dokter controller

---
+++ src/core/controllers/dokter/list.js
@@ -0,0 +1,7 @@
+let read = require("../../method/read");
+
+module.exports = (req, res, next) => {
+  read("dokter")
+    .then(list => res.status(200).json(list))
+    .catch(() => next("get list failed"));
+};
