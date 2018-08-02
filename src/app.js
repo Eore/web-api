9657e8d4cd06b5ee0fe424d1d1ffef7917355e81
Eore <eore26@gmail.com>
2018-08-02 18:35:39 +0700

    tweak app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ let http = require("http");
 let express = require("express");
 let app = express();
 let port = process.env.PORT || 8000;
-app = require("./deps/middleware")(app);
+app = require("./libs/middleware")(app);
 app = require("./core/apiRoute")(app);
 http
   .createServer(app)
