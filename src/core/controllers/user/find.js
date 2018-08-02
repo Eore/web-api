b136a53a92fa6ca56fcf023a4c3f6d11d71b57bf
Eore <eore26@gmail.com>
2018-08-02 18:44:20 +0700

    tweak find user controller

--- src/core/controllers/user/find.js
+++ src/core/controllers/user/find.js
@@ -1,10 +1,7 @@
 let find = require("../../method/find");
-let checkToken = require("../../checkToken");
-let ObjectID = require("mongodb").ObjectID;
 
 module.exports = (req, res, next) => {
-  let { _id } = checkToken(req, res);
-  find("user", { _id: ObjectID(_id) })
+  find("user", { username: req.params.username })
     .then(user => res.status(200).json({ ...user, password: "*secret*" }))
     .catch(() => next("get user failed"));
 };
