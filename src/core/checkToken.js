a7f92d4b716facc57d9f7ae877026b05751e705a
Eore <eore26@gmail.com>
2018-08-02 18:37:40 +0700

    add checkToken method

---
+++ src/core/checkToken.js
@@ -0,0 +1,11 @@
+let { verifyToken } = require("../libs/token");
+
+module.exports = (req, res) => {
+  let token = req.headers.user_token;
+  try {
+    verifyToken(token);
+  } catch (error) {
+    res.status(401).json("token expired");
+  }
+  return verifyToken(token);
+};
