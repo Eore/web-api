f884da59cba93c5674ce0201cf0ec7d75e3a7b3f
Eore <eore26@gmail.com>
2018-08-02 18:41:15 +0700

    tweak add user controller

--- src/core/controllers/user/add.js
+++ src/core/controllers/user/add.js
@@ -1,11 +1,16 @@
 let User = require("../../models/user");
-let db = require("../../../deps/database");
+let insert = require("../../method/insert");
+let find = require("../../method/find");
 
 module.exports = (req, res, next) => {
-  let newUser = new User(req.body);
-  if (newUser.data)
-    db("user")
-      .then(col => col.insert(newUser.data))
-      .then(() => res.status(201).json({ ...req.body, password: "*secret*" }))
-      .catch(() => next("add user failed"));
+  let modelName = "user";
+  find(modelName, { username: req.body.username })
+    .then(
+      user =>
+        !user
+          ? insert(modelName, User, req.body)
+          : res.status(400).json("user exist")
+    )
+    .then(() => res.status(201).json({ ...req.body, password: "*secret*" }))
+    .catch(() => next("add user failed"));
 };
