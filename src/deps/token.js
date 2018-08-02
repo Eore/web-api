d4d57440e3fc858e72da5350a946712c446f7982
Eore <eore26@gmail.com>
2018-08-02 18:43:15 +0700

    rename to libs

--- src/deps/token.js
+++
@@ -1,6 +0,0 @@
-let jwt = require("jsonwebtoken");
-let key = "m1awm14w";
-
-exports.genToken = data => jwt.sign(data, key, { expiresIn: 60 * 10 });
-exports.verifyToken = token => jwt.verify(token, key);
-exports.decodeToken = token => jwt.decode(token);
