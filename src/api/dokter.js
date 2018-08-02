442fcc497316d7bcb383ce80e0d1b1c97b122f7a
Eore <eore26@gmail.com>
2018-08-02 18:36:13 +0700

    tweak api route dokter

--- src/api/dokter.js
+++ src/api/dokter.js
@@ -1,26 +1,32 @@
 module.exports = [
-  // {
-  //   method: "GET",
-  //   url: "/dokter",
-  //   controller: "user/list",
-  //   privillage: ["admin"]
-  // },
-  // {
-  //   method: "GET",
-  //   url: "/user/:username",
-  //   controller: "user/find",
-  //   privillage: ["admin"]
-  // },
+  {
+    method: "GET",
+    url: "/dokter",
+    controller: "dokter/list",
+    privillage: ["*"]
+  },
+  {
+    method: "GET",
+    url: "/dokter/:id_dokter",
+    controller: "dokter/find",
+    privillage: ["*"]
+  },
   {
     method: "POST",
     url: "/dokter",
     controller: "dokter/add",
-    privillage: ["user"]
-  }
+    privillage: ["admin"]
+  },
   // {
   //   method: "PUT",
   //   url: "/user/:username",
   //   controller: "user/edit",
   //   privillage: ["user"]
   // }
+  {
+    method: "DELETE",
+    url: "/dokter/:id_dokter",
+    controller: "dokter/delete",
+    privillage: ["admin"]
+  }
 ];
