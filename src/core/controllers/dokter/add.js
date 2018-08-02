6bd0602e5b06f717c3dd9bdeaeb2d64e2fa8296a
Eore <eore26@gmail.com>
2018-08-02 18:38:59 +0700

    tweak add  dokter controlller

--- src/core/controllers/dokter/add.js
+++ src/core/controllers/dokter/add.js
@@ -1,11 +1,8 @@
 let Dokter = require("../../models/dokter");
-let db = require("../../../deps/database");
+let insert = require("../../method/insert");
 
 module.exports = (req, res, next) => {
-  let newDokter = new Dokter(req.body);
-  if (newDokter.data)
-    db("dokter")
-      .then(col => col.insert(newDokter.data))
-      .then(() => res.status(201).json(req.body))
-      .catch(() => next("add user failed"));
+  insert("dokter", Dokter, req.body)
+    .then(() => res.status(201).json(req.body))
+    .catch(() => next("add dokter failed"));
 };
