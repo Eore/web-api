418a9f83c60ae4bcdab4d118987316e19a56028b
Eore <eore26@gmail.com>
2018-09-04 17:16:02 +0700

    delete file

--- src/controllers/info/delete.js
+++
@@ -1,9 +0,0 @@
-let del = require("../../methods/delete");
-let ObjectID = require("mongodb").ObjectID;
-
-module.exports = (req, res, next) => {
-  let id_info = req.params.id_info;
-  del("info", { _id: ObjectID(id_info) })
-    .then(() => res.status(200).json("deleted"))
-    .catch(() => next("delete info failed"));
-};
