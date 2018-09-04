419b938e48dc3c239fe3e0e79b390d817a77288f
Eore <eore26@gmail.com>
2018-09-04 17:16:11 +0700

    tweak

--- src/controllers/dokter/editDokter.js
+++ src/controllers/dokter/editDokter.js
@@ -5,5 +5,5 @@ module.exports = (req, res) => {
   let data = req.body;
   editDokter(id_dokter, data)
     .then(doc => res.status(200).json(doc.value))
-    .catch(err => res.status(400).json(err));
+    .catch(() => res.status(400).json("gagal mengedit dokter"));
 };
