a1dc7652cfe32c84e64c0243d9f6d570f2389d0b
Eore <eore26@gmail.com>
2018-09-04 17:09:24 +0700

    change route

--- src/api/info.js
+++ src/api/info.js
@@ -5,28 +5,28 @@ module.exports = [
   //   controller: "info/list",
   //   privillage: ["*"]
   // },
-  // {
-  //   method: "GET",
-  //   url: "/info/:id_info",
-  //   controller: "info/find",
-  //   privillage: ["*"]
-  // },
+  {
+    method: "GET",
+    url: "/api/v1/info/:id_info?",
+    controller: "info/listInfo",
+    privillage: ["*"]
+  },
   {
     method: "POST",
     url: "/api/v1/info",
     controller: "info/tambahInfo",
     privillage: ["admin"]
-  }
+  },
   // {
   //   method: "PUT",
   //   url: "/info/:id_info",
   //   controller: "info/edit",
   //   privillage: ["user"]
   // },
-  // {
-  //   method: "DELETE",
-  //   url: "/info/:id_info",
-  //   controller: "info/delete",
-  //   privillage: ["admin"]
-  // }
+  {
+    method: "DELETE",
+    url: "/api/v1/info/:id_info",
+    controller: "info/hapusInfo",
+    privillage: ["admin"]
+  }
 ];
