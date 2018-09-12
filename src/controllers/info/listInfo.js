74eab60c1a56faad4cdac3c1af9f565c07a6777a
Eore <eore26@gmail.com>
2018-09-12 13:47:59 +0700

    add filter by label

--- src/controllers/info/listInfo.js
+++ src/controllers/info/listInfo.js
@@ -2,8 +2,14 @@ let { listInfo } = require("../../repository/info");
 
 module.exports = (req, res) => {
   let idInfo = req.params.id_info;
-  let { from, limit, byhit } = req.query;
-  listInfo(idInfo ? idInfo : null, from, limit, byhit)
+  let { from, limit, byhit, label } = req.query;
+  listInfo({
+    idInfo,
+    from,
+    limit,
+    byhit,
+    label
+  })
     .then(doc => res.status(200).json(doc))
     .catch(() => res.status(400).json("gagal menampilkan list info"));
 };
