dbd48da82a44fa5f40d87ea3702871a7487b46ed
Eore <eore26@gmail.com>
2018-10-23 10:53:45 +0700

    change port

--- src/module/server.js
+++ src/module/server.js
@@ -5,7 +5,7 @@ let app = express();
 let rootDir = process.cwd();
 let apiDir = rootDir + "/src/api";
 let controllerDir = rootDir + "/src/controllers";
-let port = process.env.PORT || 8000;
+let port = process.env.PORT || 3000;
 
 app.use("/asset", express.static(rootDir + "/storage"));
 app = require("./middleware")(app);
