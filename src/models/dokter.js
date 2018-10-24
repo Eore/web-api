a2c50fa900351c324e2206d826c6d7051031d1c0
Eore <eore26@gmail.com>
2018-10-24 14:45:40 +0700

    fix bug input minggu

--- src/models/dokter.js
+++ src/models/dokter.js
@@ -2,7 +2,7 @@ exports.Dokter = {
   nama: /^[A-z ,.()]{1,}$/,
   spesialis: /^[A-z, ]{1,}$/,
   jadwal: {
-    hari: /\bsenin\b|\bselasa\b|\brabu\b|\bkamis\b|\bjumat\b|\bsabtu\b/,
+    hari: /\bsenin\b|\bselasa\b|\brabu\b|\bkamis\b|\bjumat\b|\bsabtu\b|\bminggu\b/,
     mulai: /\b\d+\.\d+\b/,
     selesai: /(\b\d+\.\d+\b)|(\bselesai\b)/
   }
