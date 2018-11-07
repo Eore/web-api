a1fb24d85dfd8828c10be543f8a5d8158f9c8b76
Eore <eore26@gmail.com>
2018-11-07 10:10:00 +0700

    add tcp4 host

--- src/module/server.js
+++ src/module/server.js
@@ -1,4 +1,4 @@
-let http = require("http");
+let https = require("https");
 let express = require("express");
 let app = express();
 let fs = require("fs");
@@ -13,6 +13,12 @@ app = require("./middleware")(app);
 app = require("./router")(app, apiDir, controllerDir);
 
 exports.start = () =>
-  http
-    .createServer(app)
-    .listen(port, () => console.log(`Server berjalan di ${port}`));
+  https
+    .createServer(
+      {
+        cert: process.cwd() + "/fullchain.pem",
+        key: process.cwd() + "/privkey.pem"
+      },
+      app
+    )
+    .listen(port, "0.0.0.0", () => console.log(`Server berjalan di ${port}`));
